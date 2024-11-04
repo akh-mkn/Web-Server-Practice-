7b65abc00f5e93d4486d7c5f0e49cec669cd10af
akh-mkn <mattikn01@gmail.com>
2024-11-04 21:03:54 +0000

    update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,14 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
-
+app.get('/musicians', async (req, res) => {
+    try {
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 
 
